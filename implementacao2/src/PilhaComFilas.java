7b286f1473eca9cefd09d58030e2bb25b9e24eaa
Thiago Santiago <t_vps20@gmail.com>
2017-02-21 15:34:35 -0300

    Implementação dos metodos abstratos

--- implementacao2/src/PilhaComFilas.java
+++ implementacao2/src/PilhaComFilas.java
@@ -1,6 +1,31 @@
-public class PilhaComFilas {
+public class PilhaComFilas implements Pilha_IF {
 
     public PilhaComFilas() {
+    }
+
+    @Override
+    public void push(int element) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int pop() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int top() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isFull() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
