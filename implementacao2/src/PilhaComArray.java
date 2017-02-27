50394d4c81deae8b1cc80bc164cd03e088ea8afb
Thiago Santiago <t_vps20@gmail.com>
2017-02-27 13:42:32 -0300

    Implementado os metodos

--- implementacao2/src/PilhaComArray.java
+++ implementacao2/src/PilhaComArray.java
@@ -1,28 +1,58 @@
-public class PilhaComArray implements Pilha_IF {
-
+public class PilhaComArray implements Pilha_IF 
+{
+    int[] pilha;
+    int top;
+    
+    public PilhaComArray()
+    {
+        pilha = new int[10];
+        top = -1;
+    }
+    
     @Override
     public void push(int element) throws Exception {
-
+        if(isFull())
+            throw new Exception("Overflow");
+        else
+        {
+            top += 1;
+            pilha[top] = element;
+        }
     }
 
     @Override
     public int pop() throws Exception {
-        return 0;
+        if(isEmpty())
+            throw new Exception("Underflow");
+        else
+        {
+            top -= 1;
+            return pilha[top +1];
+        }
     }
 
     @Override
     public int top() throws Exception {
-        return 0;
+        if(isEmpty())
+            throw new Exception("Underflow");
+        else
+            return pilha[top];
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        if(top == -1)
+            return true;
+        else
+            return false;
     }
 
     @Override
     public boolean isFull() {
-        return false;
+        if(top == pilha.length -1)
+            return true;
+        else
+            return false;
     }
     
 }
