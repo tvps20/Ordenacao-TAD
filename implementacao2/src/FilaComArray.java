83fa29e1c986c178e530cad8719a16a24460444e
Thiago Santiago <t_vps20@gmail.com>
2017-02-27 14:16:11 -0300

    Implementado os metodos

--- implementacao2/src/FilaComArray.java
+++ implementacao2/src/FilaComArray.java
@@ -1,27 +1,69 @@
-public class FilaComArray implements Fila_IF {
-
+public class FilaComArray implements Fila_IF 
+{
+    int[] fila;
+    int cabeca;
+    int calda;
+    
+    public FilaComArray()
+    {
+        fila = new int[10];
+        cabeca = 0;
+        calda = -1;
+    }
+    
     @Override
     public void enqueue(int element) throws Exception {
-
+        if(isFull())
+            throw new Exception("Overflow");
+        else
+        {
+            calda += 1;
+            fila[calda] = element;
+        }
     }
 
+    public void troca(int[] vetor)
+    {
+        for(int i=0; i<calda; i++)
+            vetor[i] = vetor[i+1];
+    }
+    
     @Override
     public int dequeue() throws Exception {
-        return 0;
+        if(isEmpty())
+            throw new Exception("Underflow");
+        else
+        {
+            int resultado = fila[0];
+            troca(fila);
+            calda -= 1;
+            return resultado;
+        }
     }
 
     @Override
     public int head() throws Exception {
-        return 0;
+        if(isEmpty())
+            throw new Exception("Fila Vazia");
+        else
+        {
+            return fila[cabeca];
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        if(calda == -1)
+            return true;
+        else
+            return false;
     }
 
     @Override
     public boolean isFull() {
-        return false;
+        if(calda == fila.length -1)
+            return true;
+        else
+            return false;
     }  
 }
